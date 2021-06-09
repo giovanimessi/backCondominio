63da58b5ea9e5dabaca283d22ec611bac120f31c
Giovani <giovanimouradev@gmail.com>
2021-06-09 06:08:05 -0300

    Register Criado

--- routes/api.php
+++ routes/api.php
@@ -17,63 +17,61 @@ Route::get('/ping',function(){
 
 
 Route::get('/401',[AuthController::class, 'unauthorized'])->name('login');
-Route::post('/auth/login',[AuthController::class, 'login']);
+// Route::post('/auth/login',[AuthController::class, 'login']);
 Route::post('/auth/register',[AuthController::class, 'register']);
 
-Route::middleware('auth:api')->group(function(){
+// Route::middleware('auth:api')->group(function(){
 
-    Route::post('/auth/validate',[AuthController::class, 'validateToken']);
-    Route::post('/auth/logout',[AuthController::class, 'logout']);
+//     Route::post('/auth/validate',[AuthController::class, 'validateToken']);
+//     Route::post('/auth/logout',[AuthController::class, 'logout']);
 
-    //Mural de aviso
+//     //Mural de aviso
 
-    Route::get('/walls',[WallController::class, 'getAll']);
-    Route::post('/walls/{{id}}/like',[WallController::class, 'like']);
+//     Route::get('/walls',[WallController::class, 'getAll']);
+//     Route::post('/walls/{{id}}/like',[WallController::class, 'like']);
 
-    //documentos
-    Route::get('/docs',[DocController::class, 'All']);
+//     //documentos
+//     Route::get('/docs',[DocController::class, 'All']);
 
-    //livro de ocorrencias
-    Route::get('/warnings',[WarningController::class, 'getAllWarning']);
-    Route::post('/warning',[WarningController::class, 'setWarning']);
-    Route::post('/warning/file',[WarningController::class, 'warningfile']);
-
-
-    //boletos
-
-    Route::get('/bilets',[BilletsController::class, 'infoAll']);
+//     //livro de ocorrencias
+//     Route::get('/warnings',[WarningController::class, 'getAllWarning']);
+//     Route::post('/warning',[WarningController::class, 'setWarning']);
+//     Route::post('/warning/file',[WarningController::class, 'warningfile']);
 
 
-    //Achados
+//     //boletos
 
- Route::get('/foundandlost',[FoundandlostController::class, 'all']);
- Route::post('/foundandlost',[FoundandlostController::class, 'insert']);
- Route::put('/foundandlost/{id}',[FoundandlostController::class, 'update']);
+//     Route::get('/bilets',[BilletsController::class, 'infoAll']);
 
 
- //unidade
+//     //Achados
+
+//  Route::get('/foundandlost',[FoundandlostController::class, 'all']);
+//  Route::post('/foundandlost',[FoundandlostController::class, 'insert']);
+//  Route::put('/foundandlost/{id}',[FoundandlostController::class, 'update']);
 
 
- Route::get('/units/{id}',[FoundandlostController::class, 'dadosAll']);
- Route::post('/units/{id}/addperson',[FoundandlostController::class, 'insert']);
- Route::put('/units/{id}/addvehicle',[FoundandlostController::class, 'addVehicle']);
- Route::put('/units/{id}/addpet',[FoundandlostController::class, 'addpet']);
- Route::delete('/units/{id}/removeperson',[FoundandlostController::class, 'removeperson']);
- Route::delete('/units/{id}/removevehicle',[FoundandlostController::class, 'removevehicle']);
- Route::delete('/units/{id}/removepet',[FoundandlostController::class, 'removepet']);
+//  //unidade
 
 
- //Reservas
+//  Route::get('/units/{id}',[FoundandlostController::class, 'dadosAll']);
+//  Route::post('/units/{id}/addperson',[FoundandlostController::class, 'insert']);
+//  Route::put('/units/{id}/addvehicle',[FoundandlostController::class, 'addVehicle']);
+//  Route::put('/units/{id}/addpet',[FoundandlostController::class, 'addpet']);
+//  Route::delete('/units/{id}/removeperson',[FoundandlostController::class, 'removeperson']);
+//  Route::delete('/units/{id}/removevehicle',[FoundandlostController::class, 'removevehicle']);
+//  Route::delete('/units/{id}/removepet',[FoundandlostController::class, 'removepet']);
 
 
- Route::get('/reservations',[ReservationController::class, 'getReservation']);
- Route::get('/myreservation',[ReservationsController::class, 'getMyReservations']);
- Route::delete('/myreservation/{id}',[ReservationsController::class, 'delMyReservations']);
- Route::post('/myreservation/{id}',[ReservationsController::class, 'setReservations']);
- Route::get('/reservations/{id}/disabledates',[ReservationController::class, 'getDisabledates']);
- Route::get('/reservations/{id}/times',[ReservationController::class, 'getTimes']);
+//  //Reservas
 
 
+//  Route::get('/reservations',[ReservationController::class, 'getReservation']);
+//  Route::get('/myreservation',[ReservationsController::class, 'getMyReservations']);
+//  Route::delete('/myreservation/{id}',[ReservationsController::class, 'delMyReservations']);
+//  Route::post('/myreservation/{id}',[ReservationsController::class, 'setReservations']);
+//  Route::get('/reservations/{id}/disabledates',[ReservationController::class, 'getDisabledates']);
+//  Route::get('/reservations/{id}/times',[ReservationController::class, 'getTimes']);
 
 
 
@@ -89,4 +87,6 @@ Route::middleware('auth:api')->group(function(){
 
 
 
-});
+
+
+// });
